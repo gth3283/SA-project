a1e6b04d9a8e0e217e5f0da507990b007a20e14b
Yu-chaewon <ycw6414@naver.com>
2024-11-04 23:09:22 +0900

    타이틀-메인 씬 전환
    For Fade 대신 SceneChanger 프리팹 사용

--- 호접지몽/Assets/Scripts/SceneChange.cs
+++ 호접지몽/Assets/Scripts/SceneChange.cs
@@ -1,36 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static Unity.Collections.AllocatorManager;
 
 public class SceneChange : MonoBehaviour
 {
     private FadeManager Fade;
-    bool flag;
+    public GameObject Loading;
+    public Text LoadingText;
+    private Color color;
+
+    public static SceneChange Instance
+    {
+        get { return instance; }
+    }
+    private static SceneChange instance;
+
     void Start()
     {
+        if (instance != null) { //같은 객체가 있으면 Destroy
+            DestroyImmediate(this.gameObject);
+            return;
+        }
+        instance = this;
         Fade = FindObjectOfType<FadeManager>();
         DontDestroyOnLoad(gameObject);
-        flag = false;
     }
+    public void ChangeScene(string sceneName) //씬 전환
+    {
+        StartCoroutine(FICoroutine());
+        StartCoroutine(LoadScene(sceneName)); //로딩 시작
+    }
+
     IEnumerator FICoroutine()
     {
-        yield return new WaitForSecondsRealtime(3f);
+        color = Fade.black.color;
+        while (color.a < 1f)
+        {
+            color.a += 0.002f;
+            Fade.black.color = color;
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        yield return new WaitForSeconds(0.6f);
+        Loading.SetActive(true); //로딩창 활성화
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName); //비동기 씬 로딩 시작
+        async.allowSceneActivation = false;
+
+        float past_time = 0; //경과 시간
+        float percentage = 0; //로딩 퍼센트
+
+        while (!(async.isDone)) //씬이 로딩중이라면 반복
+        {
+            yield return null;
+            past_time += Time.deltaTime;
+            if(percentage >= 90) {
+                percentage = Mathf.Lerp(percentage, 100, past_time);
+                if(percentage == 100)
+                    async.allowSceneActivation = true; //다음 씬 표시
+            }
+            else
+            {
+                percentage = Mathf.Lerp(percentage, async.progress * 100f, past_time);
+                if(percentage >= 90)
+                    past_time = 0;
+            }
+            LoadingText.text = percentage.ToString("0") + "%"; //퍼센트 표기
+        }
+        Loading.SetActive(false); //로딩창 끄기
+        color = Fade.black.color;
+        color.a = 1f;
+        Fade.black.color = color;
         Fade.FadeIn();
-        yield return new WaitForSeconds(1.5f);
-        Destroy(gameObject);
     }
 
-    IEnumerator MainSceneLoad()
-    {
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene("Main Scene");
-    }
-
-    public void titleToMainScene()
-    {
-        Fade.FadeOut();
-        StartCoroutine(MainSceneLoad());
-        StartCoroutine(FICoroutine());
-    }
 }
