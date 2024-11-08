abc397c268d4030bd0be9344169183c8fe5857db
Yu-chaewon <ycw6414@naver.com>
2024-11-09 03:44:59 +0900

    인벤토리 추가

---
+++ 호접지몽/Assets/Scripts/ObjectItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObjectItem
+{
+    Item ClickItem();
+}
+
+public class ObjectItem : MonoBehaviour, IObjectItem
+{
+    [Header("아이템")]
+    public Item item;
+    [Header("아이템 이미지")]
+    public SpriteRenderer itemImage;
+
+    void Start()
+    {
+        itemImage.sprite = item.itemImage;
+    }
+    public Item ClickItem()
+    {
+        return this.item;
+    }
+}
