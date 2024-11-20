584881cd6240563bae866fcbb053b9d47df13740
Fun1234 <gth3283@gmail.com>
2024-11-20 18:51:45 +0900

    BGM 추가, map 4, 5 이벤트 추가, 아이, 나무 상호작용 추가, 전체 맵 벽 작업 완료

--- 호접지몽/Assets/Scripts/Title/BackgroundSoundVolumeSlider.cs
+++ 호접지몽/Assets/Scripts/Title/BackgroundSoundVolumeSlider.cs
@@ -14,7 +14,10 @@ public class BackgroundSoundVolumeSlider : MonoBehaviour
         // 슬라이더 값 초기 설정
         volumeSlider.minValue = 0f;
         volumeSlider.maxValue = 100f;
-        volumeSlider.value = BackgroundSound.volume * 100; // 현재 볼륨으로 슬라이더 초기화
+        volumeSlider.value = BackgroundSound.volume * 100;
+
+        PlayerPrefs.SetFloat("SoundVolume", BackgroundSound.volume);
+        PlayerPrefs.Save(); // 현재 볼륨으로 슬라이더 초기화
 
         // 슬라이더 값 변경 시 실행할 함수 등록
         volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
@@ -23,6 +26,6 @@ public class BackgroundSoundVolumeSlider : MonoBehaviour
     // 슬라이더 값 변경 시 호출되는 함수
     void OnVolumeChanged(float value)
     {
-        BackgroundSound.volume = value / 100f; // 슬라이더 값을 0~1 범위로 변환
+        BackgroundSound.volume=value / 100f; // 슬라이더 값을 0~1 범위로 변환
     }
 }
