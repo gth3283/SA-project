7a5de9f62ab84d403a867666af3c7a29e1e5b9f6
CranberryCereal <jcid0731@naver.com>
2024-11-25 18:34:28 +0900

    김씨 이미지업데이트 이전 세이브

--- 호접지몽/Assets/Scripts/Title/SoundEffectVolumeSlider.cs
+++ 호접지몽/Assets/Scripts/Title/SoundEffectVolumeSlider.cs
@@ -10,8 +10,10 @@ public class SoundEffectVolumeSlider: MonoBehaviour
 
     void Start()
     {
-        soundEffect = FindObjectOfType<AudioManager>();
-        soundEffect.GetVolume();
+        //soundEffect = FindObjectOfType<AudioManager>();
+        //soundEffect.GetVolume();
+
+
         volumeSlider.interactable = true; // 슬라이더 활성화
         // 슬라이더 값 초기 설정
         volumeSlider.minValue = 0f;
